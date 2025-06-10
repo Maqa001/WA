9c628a816d75653d20d4f0158baff6e1faffec1f
i-am-Melikov <mahammadmustafaam@code.edu.az>
2025-06-10 10:56:32 +0400

    1

---
+++ Areas/Manage/Controllers/IndexController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WA.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class IndexController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WA.Models;
+
+namespace WA.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ILogger<ContactController> _logger;
+
+        public ContactController(ILogger<ContactController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
